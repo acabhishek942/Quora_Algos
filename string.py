67f78136a91c19a291f28e84c3848a47713c296e
abhishek choudhary <acabhishek942@gmail.com>
2015-03-11 23:36:36 +0530

    added knuth morris pratt algo for pattern searching

--- string.py
+++ string.py
@@ -6,8 +6,31 @@ def suffix_array(string):
         suffixes.append(string[i:])
     
     for i in sorted(suffixes):
-        suffix_array.append(suffixes.index(i)
+        suffix_array.append(suffixes.index(i))
     return suffix_array
 
 
+class KnuthMorrisPratt():
+    def computeShifts(pattern):
+        shifts = [None] * (len(pattern) + 1)
+        shift = 1
+        for pos in range(len(pattern) + 1):
+            while shift < pos and pattern[pos-1] != pattern[pos-shift-1]:
+                shift += shifts[pos-shift-1]
+            shifts[pos] = shift
+        return shifts
+
+    def kmpAllMatches(pattern, text):
+        shift = computeShifts(pattern)
+        startPos = 0
+        matchLen = 0
+        for c in text:
+            while matchLen >= 0 and pattern[matchLen] != c:
+                startPos += shift[matchLen]
+                matchLen -= shift[matchLen]
+            matchLen += 1
+            if matchLen == len(pattern):
+                yield startPos
+                startPos += shift[matchLen]
+                matchLen -= shift[matchLen]
 
